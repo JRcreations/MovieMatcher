3042306e1edfd481835cd4d8074120d38fb54f01
John Reichel <jreichel447@gmail.com>
2023-01-21 17:47:29 -0600

    Initial Commit

---
+++ MovieMatcher/MovieMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatcherApp.swift
+//  MovieMatcher
+//
+//  Created by John Reichel on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
