3b3735d72a7f180e367b01c7f9fb74b53d749c90
John Reichel <jreichel447@gmail.com>
2023-02-10 10:48:10 -0600

    Updated

---
+++ MovieMatcher/Data.swift
@@ -0,0 +1,20 @@
+//
+//  Data.swift
+//  MovieMatcher
+//
+//  Created by John Reichel on 1/21/23.
+//
+
+import SwiftUI
+
+struct Data: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Data_Previews: PreviewProvider {
+    static var previews: some View {
+        Data()
+    }
+}
